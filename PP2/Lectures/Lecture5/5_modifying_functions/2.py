0dae66526ec3d4e27d11028ed5833a3e6b7d9785
NurzhigitTolendi <nu_tolendi@kbtu.kz>
2025-02-22 00:59:43 +0500

    Lecture 5

---
+++ PP2/Lectures/Lecture5/5_modifying_functions/2.py
@@ -0,0 +1,13 @@
+# compile()
+
+import re
+
+text_to_match = "John's email is john.doe@example.com, and his backup is johndoe123@work.net."
+
+pattern = r'[Jj]ohn'
+
+replace_matched_with = 'Johnny'
+
+result = re.sub(pattern, replace_matched_with, text_to_match)
+
+print(result)
